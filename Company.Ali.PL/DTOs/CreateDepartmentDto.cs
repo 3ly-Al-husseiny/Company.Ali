51d1a1832478f49572bfeb381928043f0fd524ce
3lawy <3lawy@DESKTOP-BOJ8D1T>
2025-03-09 03:17:18 +0200

    Complete The Task To-Do --> Try to Complete the Update

---
+++ Company.Ali.PL/DTOs/CreateDepartmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Ali.PL.DTOs
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage = "Code is Rquired!")]
+        public string Code { get; set; }
+        [Required(ErrorMessage ="Name is Rquired!")]
+        public string Name { get; set; }
+        
+        
+        [Required(ErrorMessage ="CreateAt is Required!")]
+        public DateTime CreateAt { get; set; }
+    }
+}
--- Company.Ali.PL/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Company.Ali.PL.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
