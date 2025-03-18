e502394e081fa32b621949b43c9e6c861ed3dc2f
3lawy <3lawy@DESKTOP-BOJ8D1T>
2025-03-18 16:56:02 +0200

    Fix Errors Finsh the Task ReFactoring Using the AutoMapper Add the Department Section

--- Company.Ali.PL/Controllers/HomeController.cs
+++ Company.Ali.PL/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using System.Text;
+using Company.Ali.PL.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Ali.PL.Controllers;
@@ -6,10 +8,45 @@ namespace Company.Ali.PL.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IScopedService _scopedService01;
+    private readonly IScopedService _scopedService02;
+    private readonly ITransientService _transientService01;
+    private readonly ITransientService _transientService02;
+    private readonly ISingltonServices _singltonServices01;
+    private readonly ISingltonServices _singltonServices02;
 
-    public HomeController(ILogger<HomeController> logger)
+
+    public HomeController(
+        ILogger<HomeController> logger,
+        IScopedService scopedService01,
+        IScopedService scopedService02,
+        ITransientService transientService01,
+        ITransientService transientService02,
+        ISingltonServices singltonServices01,
+        ISingltonServices singltonServices02
+        )
     {
         _logger = logger;
+        _scopedService01 = scopedService01;
+        _scopedService02 = scopedService02;
+        _transientService01 = transientService01;
+        _transientService02 = transientService02;
+        _singltonServices01 = singltonServices01;
+        _singltonServices02 = singltonServices02;
+    }
+
+
+    public IActionResult TestLifeTime() 
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.Append($"scopedService01 :: {_scopedService01.GetGuid()}\n");
+        builder.Append($"scopedService02 :: {_scopedService02.GetGuid()}\n");
+        builder.Append($"transientService01 :: {_transientService01.GetGuid()}\n");
+        builder.Append($"transientService02 :: {_transientService02.GetGuid()}\n");
+        builder.Append($"singltonServices01 :: {_singltonServices01.GetGuid()}\n");
+        builder.Append($"singltonServices02 :: {_singltonServices02.GetGuid()}\n");
+
+        return Content(builder.ToString());
     }
 
     public IActionResult Index()
---
+++ Company.Ali.PL/Mapping/EmployeeProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Company.Ali.DAL.Models;
+using Company.Ali.PL.DTOs;
+
+namespace Company.Ali.PL.Mapping
+{
+    public class EmployeeProfile : Profile
+    {
+
+        // CLR will Create The Object ---> Dependency Injection 
+        public EmployeeProfile()
+        {
+            // CreateMap<CreateEmployeeDto, Employee>().ReverseMap(); To Enable the Reverse Mapping 
+            
+            CreateMap<CreateEmployeeDto, Employee>()
+                .ForMember(Destination => Destination.Name, Dto => {Dto.MapFrom( E =>E.Name);
+            CreateMap<Employee, CreateDepartmentDto>();
+        }
+    }
+}
